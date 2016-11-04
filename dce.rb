71a1125b2e78c90f919bdcd81a16becaa1999754
Tim Perry <tim.perry@bbc.co.uk>
2016-11-04 14:57:09 +0000

    Adding homebrew

---
+++ dce.rb
@@ -0,0 +1,10 @@
+class Dce < Formula
+  desc "Docker Compose Exec task runner"
+  homepage "https://github.com/TimPerry/dce"
+  url "https://github.com/TimPerry/dce"
+  version "1.0.0"
+
+  def install
+    cp "dce" "/usr/local/bin/dce"
+  end
+end
