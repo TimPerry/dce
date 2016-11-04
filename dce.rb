f9c297c32b5a50daabdea849a631c2018470ee6c
Tim Perry <tim.perry@bbc.co.uk>
2016-11-04 14:59:49 +0000

    Updating brew formula

--- dce.rb
+++ dce.rb
@@ -1,7 +1,7 @@
 class Dce < Formula
   desc "Docker Compose Exec task runner"
   homepage "https://github.com/TimPerry/dce"
-  url "https://github.com/TimPerry/dce"
+  url "https://github.com/TimPerry/dce.git"
   version "1.0.0"
 
   def install
